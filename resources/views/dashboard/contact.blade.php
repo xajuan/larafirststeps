625846b0deb82540f8d959e207f42be6be12036b
Xavier Juan <xavier.juan.c@gmail.com>
2026-09-02 11:01:26 +0200

    first commit

---
+++ resources/views/dashboard/contact.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('title', 'Dashboard')
+
+@section('content')
+<h1>Contact</h1>
+<p>Contactar con la empresa</p>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>@yield('title')</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
